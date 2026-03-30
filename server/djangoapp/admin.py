06d24f21e6c4f7658d7eef1f7f0821e2414b5746
Tanner Hamilton <tannerhamilton2000@gmail.com>
2026-03-29 20:38:47 -0700

    Build CarModel and CarMake Django Models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,9 +1,13 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel, CarDealer
 
 
 # Register your models here.
+admin.site.register(CarMake)
+admin.site.register(CarModel)
+admin.site.register(CarDealer)
 
+    
 # CarModelInline class
 
 # CarModelAdmin class
