7957da936a6f3dbd4f73896acbe3594c8219dfb9
Tanner Hamilton <tannerhamilton2000@gmail.com>
2026-04-01 19:13:35 -0700

    Create Django Proxy Services of Backend APIs

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,5 +10,10 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='get_cars', view=views.get_cars,name='getcars'),
+    path(route='get_dealers', view=views.get_dealerships,name='getdealers'),
+    path(route='get_dealers/<str:state>',view=views.get_dealerships,name='getdealers'),
+    path(route='get_dealer_details/<int:dealer_id>',view=views.get_dealer_details,name='get_dealer_details'),
+    path(route='get_dealer_reviews/<int:dealer_id>',view=views.get_dealer_reviews,name='get_dealer_reviews'),
+    path(route='add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-
+from .restapis import get_request, analyze_review_sentiments, post_review
 from .populate import initiate
 
 
@@ -79,18 +79,37 @@ def registration(request):
         return JsonResponse(data)
 
 
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
+def get_dealerships(request, state="All"):
+    if(state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/"+state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status":200,"dealers":dealerships})
 
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
+
+def get_dealer_reviews(request,dealer_id):
+    if dealer_id:
+        endpoint = '/fetchReviews/dealer/'+str(dealer_id)
+        reviews_json = get_request(endpoint)
+        for review_pack in reviews_json:
+            review_analysis = analyze_review_sentiments(review_pack["review"])
+            sentiment = review_analysis['sentiment']
+            review_pack["sentiment"] = sentiment
+            # review_sentiment_json[review_pack["name"]:review_analysis]
+        return JsonResponse({"status":200,"reviews":reviews_json})
+
+
 
 # Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    if dealer_id:
+        endpoint = '/fetchDealer/'+str(dealer_id)
+        dealer = get_request(endpoint)
+        return JsonResponse({"status":200,"dealer":dealer})
+    else:
+        return JsonResponse({"status":404,"message":"Dealer not found"})
+
 
 # Create a `add_review` view to submit a review
 # def add_review(request):
@@ -105,3 +124,14 @@ def get_cars(request):
     for car_model in car_models:
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels":cars})
+
+def add_review(request):
+    if(request.user.is_anonymous == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
+    else:
+        return JsonResponse({"status":403,"message":"Unauthorized"})
